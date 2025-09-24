ad5ca4a54ae5271ac36730df8b96e2d7ba8ce93c
David Martinez <11703959+eldavo88@users.noreply.github.com>
2025-09-23 20:55:08 -0600

    Cambisos minimos. en nombres y algunas funciones.

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ let amigos = [];
 
 function agregarAmigo(){
     let espacioAmigo = document.querySelector(".input-name");
-    let guardaAmigo = espacioAmigo.ariaValueMax.trim();
+    let guardaAmigo = espacioAmigo.value.trim();
+
+    if (guardaAmigo === ''){
+        alert('El campo está vacío, por favor, ingresa un nombre válido');
+        return;
+    }
 
     if(amigos.includes(guardaAmigo)){
         alert('El nombre que has proporcionado ya ha sido agregado');
